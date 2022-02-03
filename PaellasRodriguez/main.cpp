c654db0c7e70774c6109bc14c545f8171322f3d9
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2022-02-03 12:55:03 +0100

    pantalla en blanco

--- PaellasRodriguez/main.cpp
+++ PaellasRodriguez/main.cpp
@@ -1,4 +1,34 @@
-int main(int n, char* argv[])
-{
-	return 0;
+// Grupo formado por José Miguel Villacañas y Rodrigo Cabello
+
+#include <iostream>
+#include "Game.h"
+#include "ViewController.h"
+//#include "checkML.h"
+
+using namespace std;
+
+
+void exampleLeak() {
+    int* a = new int(5);
 }
+
+int main(int argc, char* argv[]) {
+
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
+
+    try {
+        Game* game = new Game(800, 320);
+
+        ViewController controller(game);
+        controller.run();
+        delete game;
+    }
+    catch (const string& e) {
+        cout << e;
+    }
+
+    exampleLeak(); // You can remove this line
+    return 0;
+}
+
+
