f1df7eb1c52d11fbbcbac0713a7f7e8576c71e4e
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2022-03-12 13:39:18 +0100

    tabla procesado

--- GameObjects/Muebles/TablaProcesado.cpp
+++ GameObjects/Muebles/TablaProcesado.cpp
@@ -3,3 +3,14 @@
 TablaProcesado::TablaProcesado(Game* game, Vector2D<double> pos) : Mueble(game, pos, TILE_SIZE, 2 * TILE_SIZE, "tablaProcesado")
 {
 }
+
+void TablaProcesado::onCollision()
+{
+	ingr->procesando();
+	tablaLLena = true;
+}
+
+bool TablaProcesado::getTablaLlena()
+{
+	return tablaLLena;
+}
--- GameObjects/Muebles/TablaProcesado.h
+++ GameObjects/Muebles/TablaProcesado.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Mueble.h"
+#include "../Ingrediente.h"
 
 class Game;
 
@@ -7,11 +8,17 @@ class TablaProcesado: public Mueble
 {
 	const unsigned int DIMENSION_W = 1;
 	const unsigned int DIMENSION_H = 1;
+
+	Ingrediente* ingr;
+
+	bool tablaLLena = false;
+
 public:
 	TablaProcesado(Game* game, Vector2D<double> pos);
 	~TablaProcesado() = default;
 
 	//void update();
-	//void onCollision();
+	void onCollision();
+	bool getTablaLlena();
 };
 
