b577f2091c828a3c00a97f3718ce4ac8049c5e68
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2022-03-14 10:01:26 +0100

    lavavajillas

--- GameObjects/Muebles/Lavavajillas.cpp
+++ GameObjects/Muebles/Lavavajillas.cpp
@@ -2,4 +2,25 @@
 
 Lavavajillas::Lavavajillas(Game* game, Vector2D<double> pos) : Mueble(game, pos, 2 * TILE_SIZE, TILE_SIZE, "lavavajillas")
 {
+
 }
+
+void Lavavajillas::onCollision()
+{
+	if (paelleras < MAX_PAELLERAS)
+		paelleras++;
+}
+
+bool Lavavajillas::LavavajillasLleno()
+{
+	return paelleras==MAX_PAELLERAS;
+}
+
+void Lavavajillas::metePaellera()
+{
+}
+
+void Lavavajillas::sacaPaellera()
+{
+}
+
--- GameObjects/Muebles/Lavavajillas.h
+++ GameObjects/Muebles/Lavavajillas.h
@@ -1,12 +1,28 @@
 #pragma once
 #include "Mueble.h"
+#include "../Paella.h"
 
 class Game;
 
 class Lavavajillas : public Mueble
 {
 public:
+
+	const unsigned int DIMENSION_W = 1;
+	const unsigned int DIMENSION_H = 1;
+	
+	const int MAX_PAELLERAS = 5;
+	int paelleras = 0;
+	vector<Paella> paelleras;
+
+public:
 	Lavavajillas(Game* game, Vector2D<double> pos);
 	~Lavavajillas() = default;
+
+	//void update();
+	void onCollision();
+	bool LavavajillasLleno();
+	void metePaellera();
+	void sacaPaellera();
 };
 
