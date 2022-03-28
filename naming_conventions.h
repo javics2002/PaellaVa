46c6cc699ff2369f1b1ea9dabd039821683f423c
josemiguelvdz <josemiguelv2002@gmail.com>
2022-03-28 16:27:31 +0200

    Create naming_conventions.h

---
+++ naming_conventions.h
@@ -0,0 +1,29 @@
+
+class Paella {
+private:
+	int m_PaellaState;
+protected:
+	int p_PaellaState;
+public:
+	int paellaState;
+};
+
+#define MAX_INGREDIENTES 3
+const int MAX_CLIENTES = 3;
+
+enum PaellaState {
+	PAELLASTATE_EMPTY,
+	PAELLASTATE_FILLED,
+	PAELLASTATE_FINISHED
+};
+
+class Paella;
+enum PaellaState;
+struct PaellaData;
+
+
+class Module;
+void fadeToBlack(Module* from, Module* to);
+
+enum SceneType;
+void fadeToBlack(SceneType sceneType1, SceneType sceneType2);
