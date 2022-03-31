307b0262f5fde19d75410a471c2e4e6a7322668d
josemiguelvdz <josemiguelv2002@gmail.com>
2022-03-31 12:44:04 +0200

    Update FinalCinta.cpp

--- GameObjects/Muebles/FinalCinta.cpp
+++ GameObjects/Muebles/FinalCinta.cpp
@@ -5,6 +5,7 @@
 
 FinalCinta::FinalCinta(Game* game, Vector2D<double> pos) : Mueble(game, pos, TILE_SIZE, 2 * TILE_SIZE, "basura")
 {
+	setColliderRect({ (int)getX(), (int)getY(), w, h });
 }
 
 void FinalCinta::update()
