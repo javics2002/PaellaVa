00246d21246e28ebdce5a97049c7a19ff7fa26c6
mcroche611 <69208187+mcroche611@users.noreply.github.com>
2022-04-04 18:58:41 +0200

    colliders clientes más pequeños

--- GameObjects/Cliente.cpp
+++ GameObjects/Cliente.cpp
@@ -5,6 +5,7 @@ Cliente::Cliente(Game* game) : PoolObject(game)
 {
 	setDimension(DIMENSION, DIMENSION);
 	vel.setX(2.0f);
+	setColliderRect({ (int)getX(), (int)getY(), w/2, h/2});
 }
 
 void Cliente::update()
@@ -12,7 +13,7 @@ void Cliente::update()
 	setPosition(getX() + vel.getX(), getY());
 	setColliderRect({ (int)getX(), (int)getY(), w, h });
 
-	setColliderRect({ (int)getX(), (int)getY(), w, h });
+	setColliderRect({ (int)getX(), (int)getY() + h/8, w / 2, h / 2 });
 }
 
 void Cliente::cambiaTextura(string textureN)
