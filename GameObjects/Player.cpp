88d2f0ae22ddd91d71184aba16a024ce9798d067
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2022-04-08 13:21:43 +0200

    player bien puesto
    lo siento pero alguien tendra que subirlo

--- GameObjects/Player.cpp
+++ GameObjects/Player.cpp
@@ -441,7 +441,7 @@ void Player::renderDebug(SDL_Rect* cameraRect)
 
 void Player::render(SDL_Rect* cameraRect)
 {
-	SDL_Rect dest = { getX() - getWidth() / 2, getY() + getHeight() / 2, w, h };
+	SDL_Rect dest = { getX() - getWidth() / 2, getY() - getHeight() / 2, w, h };
 	drawRender(cameraRect, dest, anims[currAnim], clip);	
 }
 
