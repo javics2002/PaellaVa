bc54da350b65bb7d266f89ef9f0e67697d72f2b4
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2022-04-09 12:06:02 +0200

    Cinta ingredientes hecha bien
    Con ayuda de Victor

--- GameObjects/Muebles/Cinta.h
+++ GameObjects/Muebles/Cinta.h
@@ -5,13 +5,13 @@ class Game;
 
 class Cinta : public Mueble
 {
-
+	SDL_Rect cintaCollider;
 public:
 	Cinta(Game* game, Vector2D<double> pos);
 	~Cinta();
 
 	SDL_Rect getCollider() override;
-
+	void setCollider(SDL_Rect r) override;
 	SDL_Rect getOverlap() override;
 };
 
