edc46294cbdae0f227ef946c02d0ea47851ed41d
ARodulfo <72988547+ARodulfo@users.noreply.github.com>
2022-04-09 12:29:08 +0200

    Creditos bebe

--- Scenes/Menu.cpp
+++ Scenes/Menu.cpp
@@ -19,7 +19,7 @@ Menu::Menu(Game* game) : Scene(game)
 	sdlutils().musics().at("musicaFondo").play();
 
 	int posIni = 260;
-	int aumento = 150;
+	int aumento = 100;
 	int w = 300;
 	int h = 120;
 	auto startButton = new UiButton(game, "start", 640, posIni, w, h);
@@ -69,7 +69,35 @@ Menu::Menu(Game* game) : Scene(game)
 
 	uiManager->addInterfaz(settingsButton);
 
-	auto exitButton = new UiButton(game, "exit", 640, posIni + aumento * 2, w, h);
+	auto creditsButton = new UiButton(game, "credits", 640, posIni + aumento * 2, w, h);
+	creditsButton->setInitialDimension(w, h);
+	creditsButton->setAction([this, creditsButton](Game* game, bool& exit) {
+		sdlutils().soundEffects().at("select").play(0, game->UI);
+		uiManager->addTween(0.9f, 1.0f, 600.0f).via(easing::exponentialOut).onStep([game, creditsButton](tweeny::tween<float>& t, float) mutable {
+			creditsButton->setDimension(t.peek() * creditsButton->getInitialWidth(), t.peek() * creditsButton->getInitialHeight());
+
+			if (t.progress() > .2f) {
+				// Settings
+
+				//Abrir pantalla
+
+				game->getUIManager()->toggleCreditos();
+
+				game->getUIManager()->desactivaBot();
+
+
+				// -Poner pantalla completa (sdlutils().toggleFullScreen();)
+				// -Volumen de musica y sonido (slides separados)
+
+				return true;
+			}
+			return false;
+			});
+		});
+
+	uiManager->addInterfaz(creditsButton);
+
+	auto exitButton = new UiButton(game, "exit", 640, posIni + aumento * 3, w, h);
 	exitButton->setInitialDimension(w, h);
 	exitButton->setAction([this, exitButton](Game* game, bool& exit) {
 		sdlutils().soundEffects().at("cancel").play(0, game->UI);
@@ -85,10 +113,12 @@ Menu::Menu(Game* game) : Scene(game)
 			return false;
 			});
 		});
+
 	uiManager->addInterfaz(exitButton);
 
-
 	uiManager->creaMenuOpciones();
+
+	uiManager->creaPantallaCreditos();
 }
 
 void Menu::handleInput(bool& exit)
